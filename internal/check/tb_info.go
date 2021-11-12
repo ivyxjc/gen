b213cf60f82883aa4dfd3a9264fbaa00eb96cb86
ivyxjc <ivyxjc1994@hotmail.com>
2021-11-12 16:18:41 +0800

    feat(): support postgresql get table columns

--- internal/check/tb_info.go
+++ internal/check/tb_info.go
@@ -2,24 +2,33 @@ package check
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 
+	"fmt"
 	"gorm.io/gorm"
 
 	"gorm.io/gen/internal/model"
 )
 
 const (
-	//query table structure
+	//query table structure (mysql)
 	columnQuery = "SELECT COLUMN_NAME,COLUMN_COMMENT,DATA_TYPE,IS_NULLABLE,COLUMN_KEY,COLUMN_TYPE,COLUMN_DEFAULT,EXTRA " +
 		"FROM information_schema.COLUMNS " +
 		"WHERE TABLE_SCHEMA = ? AND TABLE_NAME =? " +
 		"ORDER BY ORDINAL_POSITION"
 
-	//query table index
+	//query table index (mysql)
 	indexQuery = "SELECT TABLE_NAME,COLUMN_NAME,INDEX_NAME,SEQ_IN_INDEX,NON_UNIQUE " +
 		"FROM information_schema.STATISTICS " +
 		"WHERE TABLE_SCHEMA = ? AND TABLE_NAME =?"
+
+	//query table structure (postgresql)
+	columnQueryPg = "SELECT COLUMN_NAME AS \"COLUMN_NAME\",'' AS \"COLUMN_COMMENT\",DATA_TYPE AS \"DATA_TYPE\",IS_NULLABLE AS \"IS_NULLABLE\"," +
+		"'' AS \"COLUMN_KEY\",'varchar' AS \"COLUMN_TYPE\", COLUMN_DEFAULT AS \"COLUMN_DEFAULT\",'' AS \"EXTRA\" " +
+		"FROM information_schema.COLUMNS " +
+		"WHERE TABLE_SCHEMA = ? AND TABLE_NAME =? " +
+		"ORDER BY ORDINAL_POSITION"
 )
 
 type ITableInfo interface {
@@ -29,7 +38,14 @@ type ITableInfo interface {
 }
 
 func getITableInfo(db *gorm.DB) ITableInfo {
-	return &mysqlTableInfo{db: db}
+	switch db.Name() {
+	case "mysql":
+		return &mysqlTableInfo{db: db}
+	case "postgres":
+		return &postgresTableInfo{db: db}
+	default:
+		panic("unsupported database")
+	}
 }
 
 func getTbColumns(db *gorm.DB, schemaName string, tableName string, indexTag bool) (result []*model.Column, err error) {
@@ -65,6 +81,10 @@ type mysqlTableInfo struct {
 	db *gorm.DB
 }
 
+type postgresTableInfo struct {
+	db *gorm.DB
+}
+
 //GetTbColumns Mysql struct
 func (t *mysqlTableInfo) GetTbColumns(schemaName string, tableName string) (result []*model.Column, err error) {
 	return result, t.db.Raw(columnQuery, schemaName, tableName).Scan(&result).Error
@@ -73,4 +93,34 @@ func (t *mysqlTableInfo) GetTbColumns(schemaName string, tableName string) (resu
 //GetTbIndex Mysql index
 func (t *mysqlTableInfo) GetTbIndex(schemaName string, tableName string) (result []*model.Index, err error) {
 	return result, t.db.Raw(indexQuery, schemaName, tableName).Scan(&result).Error
+}
+
+func (t *postgresTableInfo) GetTbColumns(schemaName string, tableName string) (result []*model.Column, err error) {
+	if err = t.db.Raw(columnQueryPg, schemaName, tableName).Scan(&result).Error; err != nil {
+		return nil, err
+	}
+	if rows, err := t.db.Raw(fmt.Sprintf("select * from %s LIMIT 0", tableName)).Rows(); err != nil {
+		return nil, err
+	} else {
+		if types, err := rows.ColumnTypes(); err != nil {
+			return nil, err
+		} else {
+			m := make(map[string]*sql.ColumnType)
+			for i := range types {
+				m[types[i].Name()] = types[i]
+			}
+			for _, r := range result {
+				t := m[r.ColumnName]
+				if t.Name() == r.ColumnName {
+					r.ColumnType = t.DatabaseTypeName()
+				}
+			}
+		}
+	}
+	return result, nil
+}
+
+func (t *postgresTableInfo) GetTbIndex(schemaName string, tableName string) (result []*model.Index, err error) {
+	// todo
+	panic("Not support postgresql")
 }
